92d19196b96abed234f63433f0492d1a01de6d62
Pankaj Jangid <pankaj.jangid@gmail.com>
2019-09-22 09:01:09 +0530

    desktop session is now saved.

--- lisp/init-vars.el
+++ lisp/init-vars.el
@@ -4,5 +4,16 @@
 ;; (setq ls-lisp-use-insert-directory-program nil)
 (setq dired-use-ls-dired nil)
 
+;; default frame size
+;; (setq initial-frame-alist
+;;       '((top . 1) (left . 1) (width . 80) (height . 55)))
+
+;; save and restore desktop session 
+(desktop-save-mode 1)
+(add-to-list 'desktop-path (expand-file-name "desktop" user-emacs-directory))
+(setq desktop-dirname (expand-file-name "desktop" user-emacs-directory))
+(unless (file-exists-p desktop-dirname)
+  (make-directory desktop-dirname))
+
 (provide 'init-vars)
 ;; end-of init-vars.el
