3abca4381fb91c8e6be82522ea10c31879e6a8d6
Pankaj Jangid <pankaj.jangid@gmail.com>
2019-09-25 13:17:02 +0530

    using helm now

--- lisp/init-ivy.el
+++
@@ -1,9 +0,0 @@
-;; init-ivy.el --- ivy mode configuration -*- lexical-binding: t -*-
-
-(when (package-installed-p 'ivy)
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t))
-
-(provide 'init-ivy)
-;; end-of init-ivy.el
