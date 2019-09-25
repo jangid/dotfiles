9ae81c6d79688a555033c5050028bb9eee1fa422
Pankaj Jangid <pankaj.jangid@gmail.com>
2019-09-25 13:18:37 +0530

    removed

--- lisp/init-company.el
+++
@@ -1,7 +0,0 @@
-;; init-company.el --- completion framework -*- lexical-binding: t -*-
-
-(when (package-installed-p 'company)
-  (add-hook 'after-init-hook 'global-company-mode))
-
-(provide 'init-company)
-;; end-of init-company.el
