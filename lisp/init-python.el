3f0c563a3c793c5d6e2e7a08e145aae03936679c
Pankaj Jangid <pankaj.jangid@gmail.com>
2019-11-09 00:49:06 +0530

    removed stale files

--- lisp/init-python.el
+++
@@ -1,8 +0,0 @@
-;;; init-python.el --- python specific configuration -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(add-hook 'python-mode-hook #'lsp)
-
-(provide 'init-python)
-;;; init-python.el ends here
