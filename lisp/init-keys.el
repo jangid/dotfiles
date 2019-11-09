47561c830edea4ab2e35b3e787affa42da696ddb
Pankaj Jangid <pankaj.jangid@gmail.com>
2019-11-09 18:03:43 +0530

    Changed macOS keys to match it with Windows/Linux keyboard

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -1,8 +1,9 @@
 ;;; init-keys.el --- customize keyboard behaviour -*- lexical-binding: t -*-
 
-;; (setq mac-command-modifier 'meta)
-;; (setq mac-option-modifier nil)
-;; (setq mac-right-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+(setq mac-right-option-modifier nil)
+(setq mac-right-command-modifier nil)
 
 (add-hook 'after-init-hook #'which-key-mode)
 
