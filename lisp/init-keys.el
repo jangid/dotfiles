5ca8d79f34e0c875b46534ad96f43b8c4a2b14fc
Pankaj Jangid <pankaj.jangid@gmail.com>
2019-12-10 19:00:31 +0530

    Swapped meta and super keys.

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -1,7 +1,7 @@
 ;;; init-keys.el --- customize keyboard behaviour -*- lexical-binding: t -*-
 
-;; (setq mac-command-modifier 'meta)
-;; (setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
 (setq mac-right-option-modifier nil)
 (setq mac-right-command-modifier nil)
 
