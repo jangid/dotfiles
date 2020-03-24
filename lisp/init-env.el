39984033b2ccfcc837988193e89abc701144f770
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-03-24 12:56:09 +0530

    Configured time-zone and location for New Delhi

--- lisp/init-env.el
+++ lisp/init-env.el
@@ -12,5 +12,14 @@
 (setq user-mail-address	"p4j@j4d.net"
       user-full-name "Pankaj Jangid")
 
+;; time-zone
+(eval-when-compile (require 'solar))
+(setq calendar-latitude 28.6)		; New Delhi
+(setq calendar-longitude 77.3)
+(setq calendar-location-name "New Delhi, India")
+(setq calendar-time-zone +330)
+(setq calendar-standard-time-zone-name "IST")
+(setq calendar-daylight-time-zone-name "IST")
+
 (provide 'init-env)
 ;;; init-env.el ends here
