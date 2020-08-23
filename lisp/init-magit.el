62cd600e9776879ef7c31f1f56e48aacc1251bd5
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-08-23 11:28:53 +0530

    added magit global keybindings

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -7,5 +7,8 @@
 (use-package magit
   :ensure t)
 
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
 (provide 'init-magit)
 ;;; init-magit.el ends here
