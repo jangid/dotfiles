0c1b7d39b493d1fa3de0acdf17dfa788eea81c73
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-08-23 11:20:32 +0530

    separated which-key

---
+++ lisp/init-which-key.el
@@ -0,0 +1,12 @@
+;;; init-which-key.el --- configure which-key -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'init-pkg)
+
+;; Get help for key bindings
+(use-package which-key
+  :ensure t)
+
+(provide 'init-which-key)
+;;; init-which-key.el ends here
