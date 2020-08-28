af811adc0bf2dd7edc0439f87a091586d6fb4c93
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-08-28 18:03:33 +0530

    cleaned up rust config

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -4,27 +4,21 @@
 
 (require 'init-use-package)
 
-(use-package company
-  :ensure t
-  :hook
-  (rust-mode . company-mode))
-
 (use-package rust-mode
-  :ensure t)
-
-(use-package eglot
   :ensure t
-  :hook
-  (rust-mode . eglot-ensure))
-
-(use-package flymake
-  :ensure t)
-
-(defun my/rust-mode-hook ()
-  "Custom code to run on start of 'rust-mode'."
-  (setq indent-tabs-mode nil))
-
-(add-hook 'rust-mode-hook 'my/rust-mode-hook)
+  :init
+  (setq indent-tabs-mode nil)
+  :config
+  (use-package company
+    :ensure t
+    :hook
+    (rust-mode . company-mode))
+  (use-package flymake
+    :ensure t)
+  (use-package eglot
+    :ensure t
+    :hook
+    (rust-mode . eglot-ensure)))
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
