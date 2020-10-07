50b3908a4a442dda02e9947e3c5c8187c0848d4a
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-10-07 22:51:24 +0530

    Added degree, minute, second to decimal degrees conversion formula in comments.

--- lisp/init-date-time.el
+++ lisp/init-date-time.el
@@ -1,5 +1,12 @@
 ;;; init-date-time.el --- date, time, calendar config -*-lexical-mode: t-*-
 ;;; Commentary:
+
+;; 1 minute is equal to 60 seconds.  1 degree is equal to 1 hour, that
+;; is equal to 60 minutes or 3600 seconds.  To calculate decimal
+;; degrees, we use the DMS to decimal degree formula below:
+;;
+;; Decimal Degrees = degrees + (minutes/60) + (seconds/3600)
+
 ;;; Code:
 
 ;; time-zone
@@ -10,8 +17,10 @@
 ;; (setq calendar-longitude 77.3)
 ;; (setq calendar-location-name "New Delhi, India")
 
-(setq calendar-latitude 26.9)		; Jaipur
-(setq calendar-longitude 77.8)
+;; (setq calendar-latitude 26.9)		; Jaipur
+;; (setq calendar-longitude 75.8)
+(setq calendar-latitude [26 52 north])		; Jaipur
+(setq calendar-longitude [75 46 east])
 (setq calendar-location-name "Jaipur, India")
 
 (setq calendar-time-zone +330)
