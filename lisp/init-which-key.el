20f9982fe062e815ce311165f1b65b12138f6f50
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-10-09 22:23:26 +0530

    Added submodule for which-key. Not sure why it is not working when loaded using packge.

--- lisp/init-which-key.el
+++ lisp/init-which-key.el
@@ -9,17 +9,10 @@
 (require 'package)
 (defun my/which-key-setup ()
   "Startup configuration for which-key."
-  ;; (unless (package-installed-p 'which-key)
-  ;;   (package-install 'which-key))
-
   (add-to-list 'load-path (expand-file-name
-			   "emacs-which-key/which-key.el"
+			   "emacs-which-key"
 			   user-emacs-directory))
   (require 'which-key)
-  (which-key-mode)
-  ;; (require 'which-key)
-  ;; (declare-function which-key-setup-side-window-right-bottom "which-key")
-  ;; (which-key-setup-side-window-right-bottom)
   (declare-function which-key-mode "which-key")
   (which-key-mode 1))
 
