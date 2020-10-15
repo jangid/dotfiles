3fb339d67bf2b16eb03248319a585a653ab237ac
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-10-15 15:43:48 +0530

    Added function (my/set-face) and (my/unset-face)

--- lisp/init-email.el
+++ lisp/init-email.el
@@ -65,23 +65,47 @@
   (interactive)
   (progn
     (declare-function message-remove-header "message.el")
-    (message-remove-header "X-Message-SMTP-Method"
-	     (my/get-smtp))))
+    (message-remove-header "X-Message-SMTP-Method")))
 
 (add-hook 'message-setup-hook 'my/set-smtp)
-  
+
+;; Make face from JPG using:
+
+;; "djpeg face.jpg | ppmnorm | pnmscale -width 48 -height 48 |
+;; ppmquant 7 | pnmtopng > face.png"
+
+;; Now check the size of "face.png" file. It should be less than 725
+;; bytes. ref: http://quimby.gnus.org/circus/face/
+
+
+(defun my/set-face ()
+  "Add Face header to email message."
+  (interactive)
+    (progn
+    (declare-function message-add-header "message.el")
+    (declare-function message-fetch-field "message.el")
+    (declare-function ietf-drums-parse-address "ietf-drums.el")
+    (message-add-header
+     (concat "Face: "
+	     (gnus-face-from-file "face.png")))))
+
+(defun my/unset-face ()
+  "Remove Face field."
+  (interactive)
+  (progn
+    (declare-function message-remove-header "message.el")
+    (message-remove-header "Face")))
+
 ;; (make-temp-file
 ;;  (expand-file-name "invite-" temporary-file-directory)
 ;;  nil
 ;;  ".ics")
 
 ;; Configure Citation
-(defvar message-cite-style)
-(setq message-cite-style
-      '((message-citation-line-function
-	 'message-insert-formatted-citation-line)))
-
-;; (add-hook 'mail-citation-hook 'sc-cite-original)
+;; (defvar message-cite-style)
+;; (setq message-cite-style
+;;       '((message-citation-line-function
+;; 	 'message-insert-formatted-citation-line)))
 
 ;; Configure search index
 (defun my/timestamp-me (process event)
