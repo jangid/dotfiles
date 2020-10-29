b92bbf387e5366714e322d5a4e4da8c5d06f5bfd
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-10-29 14:21:22 +0530

    do not use use-package for org

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,22 +2,48 @@
 ;;; Commentary:
 ;;; Code:
 
-(eval-when-compile (require 'init-use-package))
-(use-package org
-  :bind
-  (("C-c a" . org-agenda)
-   ("C-c b" . org-switchb)
-   ("C-c c" . org-capture)
-   ("C-c l" . org-store-link)
-   ("C-c f" . org-footnote-action))
-  :config
+;; (eval-when-compile (require 'init-use-package))
+;; (use-package org
+;;   :bind
+;;   (("C-c a" . org-agenda)
+;;    ("C-c b" . org-switchb)
+;;    ("C-c c" . org-capture)
+;;    ("C-c l" . org-store-link)
+;;    ("C-c f" . org-footnote-action))
+;;   :config
+;;   (defvar org-agenda-include-diary t)
+;;   (add-hook 'org-mode-hook
+;; 	    (lambda ()
+;; 	      ;; (visual-line-mode)
+;; 	      ;; (org-indent-mode)
+;; 	      ;; (require 'org-tempo)
+
+;; 	      (require 'ob-python)
+;; 	      (require 'ob-C)
+;; 	      (require 'ob-java)
+;; 	      (require 'ob-latex)
+;; 	      (require 'ob-js)
+;; 	      (require 'ob-css)
+;; 	      (require 'ob-sql)
+;; 	      (require 'ob-emacs-lisp))))
+
+(defun my-org-setup ()
+  "Organise the Org."
+
+  (global-set-key (kbd "C-c a") 'org-agenda)
+  (global-set-key (kbd "C-c b") 'org-switchb)
+  (global-set-key (kbd "C-c c") 'org-capture)
+  (global-set-key (kbd "C-c l") 'org-store-link)
+  (global-set-key (kbd "C-c f") 'org-footnote-action)
+  
   (defvar org-agenda-include-diary t)
+
   (add-hook 'org-mode-hook
 	    (lambda ()
 	      ;; (visual-line-mode)
 	      ;; (org-indent-mode)
 	      ;; (require 'org-tempo)
-
+	      
 	      (require 'ob-python)
 	      (require 'ob-C)
 	      (require 'ob-java)
@@ -26,6 +52,8 @@
 	      (require 'ob-css)
 	      (require 'ob-sql)
 	      (require 'ob-emacs-lisp))))
+
+(add-hook 'emacs-startup-hook 'my-org-setup)
 
 (provide 'init-org)
 ;;; init-org.el ends here
