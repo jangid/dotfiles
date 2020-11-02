b92ac344beed5fd564d1a658caa655cea8bb7984
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-11-03 00:53:26 +0530

    require macos contacts backend only on macos

--- lisp/init-eudc.el
+++ lisp/init-eudc.el
@@ -4,14 +4,15 @@
 
 (add-hook 'emacs-startup-hook
 	  (lambda ()
-	    (eval-and-compile (require 'eudcb-macos-contacts))
-	    (eudc-macos-contacts-set-server "localhost")
+	    (when (eq system-type 'darwin)
+	      (eval-and-compile (require 'eudcb-macos-contacts))
+	      (eudc-macos-contacts-set-server "localhost"))
 	    (eval-when-compile (require 'message))
 	    (define-key message-mode-map
 	      [(control ?c) (tab)] 'eudc-expand-inline)
 	    (eval-when-compile (require 'sendmail))
-	      (define-key mail-mode-map
-		[(control ?c) (tab)] 'eudc-expand-inline)))
+	    (define-key mail-mode-map
+	      [(control ?c) (tab)] 'eudc-expand-inline)))
 
 (provide 'init-eudc)
 ;;; init-eudc.el ends here
