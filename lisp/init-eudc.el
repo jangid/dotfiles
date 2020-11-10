82eab7be90fd8ed00260fd99aabfaaccc31d7a68
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-11-10 13:02:37 +0530

    disabled eudc

--- lisp/init-eudc.el
+++ lisp/init-eudc.el
@@ -2,14 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-hook 'emacs-startup-hook
-	  (lambda ()
-	    (when (eq system-type 'darwin)
-	      (eval-and-compile (require 'eudcb-macos-contacts))
-	      (eudc-macos-contacts-set-server "localhost"))
-	    (custom-set-variables
-	     '(eudc-server-hotlist '(("" . bbdb)))
-	     '(eudc-inline-expansion-servers 'hotlist))))
+;; (add-hook 'emacs-startup-hook
+;; 	  (lambda ()
+;; 	    (when (eq system-type 'darwin)
+;; 	      (eval-and-compile (require 'eudcb-macos-contacts))
+;; 	      (eudc-macos-contacts-set-server "localhost"))
+;; 	    (custom-set-variables
+;; 	     '(eudc-server-hotlist '(("" . bbdb)))
+;; 	     '(eudc-inline-expansion-servers 'hotlist))))
 
 (provide 'init-eudc)
 ;;; init-eudc.el ends here
