076ba93526fb8173165b24c9e32990eda6942ee3
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-11-22 22:40:24 +0530

    org gtd experiments

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -27,6 +27,25 @@
 ;; 	      (require 'ob-sql)
 ;; 	      (require 'ob-emacs-lisp))))
 
+(require 'org)
+
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+		  
+(defun org-current-is-todo ()
+  "Check if the currect node is in todo state."
+  (string= "TODO" (org-get-todo-state)))
+
 (defun my-org-setup ()
   "Organise the Org."
 
@@ -37,15 +56,43 @@
   (global-set-key (kbd "C-c f") 'org-footnote-action)
   
   (custom-set-variables
-   '(org-agenda-include-diary t)
-   '(org-agenda-files '("~/org"))
-   '(org-default-notes-file (concat org-directory "/notes.org")))
+   '(org-agenda-include-diary nil)
+   '(org-agenda-files
+     (mapcar
+      (lambda (x)
+	(concat org-directory "/" x))
+      '("notes.org" "gtd.org" "tickler.org")))
+   '(org-capture-templates
+     '(("t" "Todo" entry (file+headline "notes.org" "Tasks")
+	"* TODO %?\n %i")
+       ("T" "Tickler" entry (file+headline "tickler.org" "Tickler")
+	"* TODO %?\n %U\n %i")))
+   '(org-refile-targets
+     `((,(concat org-directory "/gtd.org") :maxlevel . 3)
+       (,(concat org-directory "/someday.org") :level . 1)
+       (,(concat org-directory "/tickler.org") :maxlevel . 2)))
+   '(org-todo-keywords
+     '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+   '(org-agenda-custom-commands
+      '(("o" "At Optimzory" tags-todo "@optimizory"
+         ((org-agenda-overriding-header "Optimizory")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))
+	("j" "At J4D" tags-todo "@j4d"
+         ((org-agenda-overriding-header "J4D")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))
+	("h" "At Home" tags-todo "@home"
+         ((org-agenda-overriding-header "Home")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))
+	("f" "At FSF" tags-todo "@fsf"
+         ((org-agenda-overriding-header "FSF")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))
+	)))
 
   (add-hook 'org-mode-hook
 	    (lambda ()
-	      (visual-line-mode)
+	      (visual-line-mode +1)
 	      (declare-function org-indent-mode "org.el")
-	      (org-indent-mode)
+	      (org-indent-mode +1)
 	      ;; (require 'org-tempo)
 	      
 	      (require 'ob-python)
