4dd160a49fcb72b414faf72665f4376342b4ef19
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-11-23 00:40:54 +0530

    ebdb play

--- lisp/init-ebdb.el
+++ lisp/init-ebdb.el
@@ -5,15 +5,22 @@
 
 ;;; Code:
 
-(eval-when-compile (require 'init-use-package))
-(use-package ebdb
-  :ensure t
-  :config
-  (setq ebdb-mua-pop-up nil)
-  :hook
-  (emacs-startup . (progn
-		     (require 'ebdb-gnus)
-		     (require 'ebdb-message))))
+;; (eval-when-compile (require 'init-use-package))
+;; (use-package ebdb
+;;   :ensure t
+;;   :config
+;;   (setq ebdb-mua-pop-up nil)
+;;   :hook
+;;   (emacs-startup . (progn
+;; 		     (require 'ebdb-gnus)
+;; 		     (require 'ebdb-message))))
+
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (progn
+	      (defvar ebdb-mua-pop-up nil)
+	      (require 'ebdb-gnus)
+	      (require 'ebdb-message))))
 
 (provide 'init-ebdb)
 ;;; init-ebdb.el ends here
