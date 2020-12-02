70137d1f2cbab3aa7aff5f4abf6d879ee00bdedf
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-12-02 10:37:23 +0530

    added some agenda files

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -48,7 +48,7 @@
 
 (defun my-org-setup ()
   "Organise the Org."
-
+  (interactive)
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c b") 'org-switchb)
   (global-set-key (kbd "C-c c") 'org-capture)
@@ -57,22 +57,31 @@
   
   (custom-set-variables
    '(org-agenda-include-diary nil)
+   '(org-directory "~/Documents/org")
    '(org-agenda-files
      (mapcar
       (lambda (x)
 	(concat org-directory "/" x))
-      '("notes.org" "gtd.org" "tickler.org")))
+      '("notes.org"
+	"gtd.org"
+	"optimizory.org"
+	"personal.org"
+	"j4d.org"
+	"social.org")))
    '(org-capture-templates
      '(("t" "Todo" entry (file+headline "notes.org" "Tasks")
 	"* TODO %?\n %i")
-       ("T" "Tickler" entry (file+headline "tickler.org" "Tickler")
+       ("T" "Tickler" entry (file+headline "gtd.org" "Ticklers")
 	"* TODO %?\n %U\n %i")))
    '(org-refile-targets
      `((,(concat org-directory "/gtd.org") :maxlevel . 3)
-       (,(concat org-directory "/someday.org") :level . 1)
-       (,(concat org-directory "/tickler.org") :maxlevel . 2)))
+       (,(concat org-directory "/optimizory.org") :maxlevel . 3)
+       (,(concat org-directory "/personal.org") :maxlevel . 3)
+       (,(concat org-directory "/j4d.org") :maxlevel . 3)
+       (,(concat org-directory "/social.org") :maxlevel . 3)
+       (,(concat org-directory "/someday.org") :level . 1)))
    '(org-todo-keywords
-     '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+     '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
    '(org-agenda-custom-commands
       '(("o" "At Optimzory" tags-todo "@optimizory"
          ((org-agenda-overriding-header "Optimizory:")
@@ -92,7 +101,7 @@
 
   (add-hook 'org-mode-hook
 	    (lambda ()
-	      (visual-line-mode +1)
+	      ;(visual-line-mode +1)
 	      (declare-function org-indent-mode "org.el")
 	      (org-indent-mode +1)
 	      ;; (require 'org-tempo)
