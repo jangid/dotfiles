bede6c6c35763290ba73e4927d2942772c9f4d05
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-12-10 23:09:04 +0530

    optimized loading time

--- lisp/init-ebdb.el
+++ lisp/init-ebdb.el
@@ -5,15 +5,17 @@
 
 ;;; Code:
 
+(setq compose-mail-user-agent-warnings nil)
+
 (eval-when-compile (require 'init-use-package))
 (use-package ebdb
   :ensure t
-  :defer t
   :init
   (defvar ebdb-mua-pop-up nil)
-  :config
-  (use-package ebdb-gnus)
-  (use-package ebdb-message))
+  :hook
+  (emacs-startup . (lambda ()
+		     (use-package ebdb-gnus)
+		     (use-package ebdb-message))))
 
 (provide 'init-ebdb)
 ;;; init-ebdb.el ends here
