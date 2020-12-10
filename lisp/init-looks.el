80d5450313d08890a59946178b154313a44602c2
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-12-10 19:55:16 +0530

    cleaned up commented lines in looks.

--- lisp/init-looks.el
+++ lisp/init-looks.el
@@ -2,37 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (setq inhibit-startup-screen t)
-;; (setq initial-scratch-message nil)
-
-;; Disable menu-bar, tool-bar and scroll-bar. What is the use of emacs
-;; if you are using these mouseee features.
-
-;; menuBar
-;; (menu-bar-mode +1)
-;;(menu-bar-mode -1)
-
-;; scrollBar
-;; (scroll-bar-mode +1)
-
-;; toolBar
-;; (tool-bar-mode +1)
-;;(tool-bar-mode -1)
-
-;; tabBar
-;; (setq tab-bar-show 1)
-;; (tab-bar-mode +1)
-;; (tab-bar-mode -1)
-
-;; cursor
-;; (blink-cursor-mode t)
-;; (global-hl-line-mode +1)
-
-;; (setq default-frame-alist
-;;       '((height . 55)
-;;  	(width . 169)
-;; 	(left . 110)))
-
 (defun modus-theme-toggle ()
   "Toggle between modus-operandi and modus-vivendi themes."
   (interactive)
