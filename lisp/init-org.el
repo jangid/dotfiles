dd52b11f19f8f40cfd8e9e37ef29121908aae4da
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-12-10 19:25:19 +0530

    cumstom commands moved to use-package for org.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,23 +12,25 @@
   :hook
   ((message-mode . orgtbl-mode))
   :config
-  (add-hook 'org-mode-hook
-	    (lambda ()
-	      (declare-function org-indent-mode "org.el")
-	      (org-indent-mode +1)
-	      
-	      (require 'ob-python)
-	      (require 'ob-C)
-	      (require 'ob-java)
-	      (require 'ob-latex)
-	      (require 'ob-js)
-	      (require 'ob-css)
-	      (require 'ob-sql)
-	      (require 'ob-emacs-lisp)
-	      (defvar org-plantuml-jar-path "/usr/local/opt/libexec/plantuml.jar")
-	      (require 'ob-plantuml)))
+  (defun my-org-mode-hook ()
+    (declare-function org-indent-mode "org.el")
+    (org-indent-mode +1))
+  (add-hook 'org-mode-hook 'my-org-mode-hook)
 
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (R . t)
+     (python . t)
+     (C . t)
+     (java . t)
+     (js . t)
+     (css . t)
+     (sql . t)
+     (plantuml . t)))
+  :init
   (defvar org-agenda-include-diary t)
+  (defvar org-plantuml-jar-path "/usr/local/opt/libexec/plantuml.jar")
   (defvar org-directory "~/Documents/org")
   (defvar org-agenda-files
     (mapcar
@@ -58,124 +60,47 @@
 		"WAITING(w)"
 		"|"
 		"DONE(d)"
-		"CANCELLED(c)"))))
-  
-  ;; (add-hook 'org-mode-hook
-  ;; 	    (lambda ()
-  ;; 	      ;(visual-line-mode +1)
-  ;; 	      (declare-function org-indent-mode "org.el")
-  ;; 	      (org-indent-mode +1)
-  ;; 	      ;; (require 'org-tempo)
+		"CANCELLED(c)")))
+  (defvar org-agenda-custom-commands
+    '(("o" "At Optimzory" tags-todo "@optimizory"
+       ((org-agenda-overriding-header "Optimizory:")
+        (org-agenda-skip-function
+         #'my-org-agenda-skip-all-siblings-but-first)))
+      ("j" "At J4D" tags-todo "@j4d"
+       ((org-agenda-overriding-header "J4D:")
+        (org-agenda-skip-function
+         #'my-org-agenda-skip-all-siblings-but-first)))
+      ("h" "At Home" tags-todo "@home"
+       ((org-agenda-overriding-header "Home and Personal:")
+        (org-agenda-skip-function
+         #'my-org-agenda-skip-all-siblings-but-first)))
+      ("f" "At FSF" tags-todo "@fsf"
+       ((org-agenda-overriding-header "FSF:")
+        (org-agenda-skip-function
+         #'my-org-agenda-skip-all-siblings-but-first)))
+      ("n" "Agenda and all TODOs"
+       ((agenda "")
+	(alltodo ""))))))
 
-  ;; 	      (require 'ob-python)
-  ;; 	      (require 'ob-C)
-  ;; 	      (require 'ob-java)
-  ;; 	      (require 'ob-latex)
-  ;; 	      (require 'ob-js)
-  ;; 	      (require 'ob-css)
-  ;; 	      (require 'ob-sql)
-  ;; 	      (require 'ob-plantuml)
-  ;; 	      (require 'ob-emacs-lisp))))
-  
-;; (eval-when-compile (require 'org))
+(require 'org)
+(require 'outline)
 
-;; (defun my-org-agenda-skip-all-siblings-but-first ()
-;;   "Skip all but the first non-done entry."
-;;   (let (should-skip-entry)
-;;     (unless (org-current-is-todo)
-;;       (setq should-skip-entry t))
-;;     (save-excursion
-;;       (while (and (not should-skip-entry) (org-goto-sibling t))
-;;         (when (org-current-is-todo)
-;;           (setq should-skip-entry t))))
-;;     (when should-skip-entry
-;;       (or (outline-next-heading)
-;;           (goto-char (point-max))))))
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
 		  
-;; (defun org-current-is-todo ()
-;;   "Check if the currect node is in todo state."
-;;   (string= "TODO" (org-get-todo-state)))
-
-;; (defun my-org-setup ()
-;;   "Organise the Org."
-;;   (interactive)
-;;   (global-set-key (kbd "C-c a") 'org-agenda)
-;;   (global-set-key (kbd "C-c b") 'org-switchb)
-;;   (global-set-key (kbd "C-c c") 'org-capture)
-;;   (global-set-key (kbd "C-c l") 'org-store-link)
-  
-;;   (custom-set-variables
-;;    '(org-agenda-include-diary t)
-;;    '(org-directory "~/Documents/org")
-;;    '(org-agenda-files
-;;      (mapcar
-;;       (lambda (x)
-;; 	(concat org-directory "/" x))
-;;       '("notes.org"
-;; 	"gtd.org"
-;; 	"optimizory.org"
-;; 	"personal.org"
-;; 	"j4d.org"
-;; 	"social.org")))
-;;    '(org-capture-templates
-;;      '(("t" "Todo" entry (file+headline "notes.org" "Tasks")
-;; 	"* TODO %?\n %i")
-;;        ("T" "Tickler" entry (file+headline "gtd.org" "Ticklers")
-;; 	"* TODO %?\n %U\n %i")))
-;;    '(org-refile-targets
-;;      `((,(concat org-directory "/gtd.org") :maxlevel . 3)
-;;        (,(concat org-directory "/optimizory.org") :maxlevel . 3)
-;;        (,(concat org-directory "/personal.org") :maxlevel . 3)
-;;        (,(concat org-directory "/j4d.org") :maxlevel . 3)
-;;        (,(concat org-directory "/social.org") :maxlevel . 3)
-;;        (,(concat org-directory "/someday.org") :level . 1)))
-;;    '(org-todo-keywords
-;;      '((sequence "TODO(t)"
-;; 		 "NEXT(n)"
-;; 		 "WAITING(w)"
-;; 		 "|"
-;; 		 "DONE(d)"
-;; 		 "CANCELLED(c)")))
-;;    ;; '(org-agenda-custom-commands
-;;    ;;   '(("o" "At Optimzory" tags-todo "@optimizory"
-;;    ;; 	((org-agenda-overriding-header "Optimizory:")
-;;    ;;       (org-agenda-skip-function
-;;    ;;        #'my-org-agenda-skip-all-siblings-but-first)))
-;;    ;;     ("j" "At J4D" tags-todo "@j4d"
-;;    ;;      ((org-agenda-overriding-header "J4D:")
-;;    ;;       (org-agenda-skip-function
-;;    ;;        #'my-org-agenda-skip-all-siblings-but-first)))
-;;    ;;     ("h" "At Home" tags-todo "@home"
-;;    ;;      ((org-agenda-overriding-header "Home and Personal:")
-;;    ;;       (org-agenda-skip-function
-;;    ;;        #'my-org-agenda-skip-all-siblings-but-first)))
-;;    ;;     ("f" "At FSF" tags-todo "@fsf"
-;;    ;;      ((org-agenda-overriding-header "FSF:")
-;;    ;;       (org-agenda-skip-function
-;;    ;;        #'my-org-agenda-skip-all-siblings-but-first)))
-;;    ;;     ("n" "Agenda and all TODOs"
-;;    ;; 	((agenda "")
-;;    ;; 	 (alltodo "")))))
-;;    '(org-plantuml-jar-path "/usr/local/opt/libexec/plantuml.jar"))
-  
-;;   (add-hook 'org-mode-hook
-;; 	    (lambda ()
-;; 	      ;(visual-line-mode +1)
-;; 	      (declare-function org-indent-mode "org.el")
-;; 	      (org-indent-mode +1)
-;; 	      ;; (require 'org-tempo)
-
-;; 	      (require 'ob-python)
-;; 	      (require 'ob-C)
-;; 	      (require 'ob-java)
-;; 	      (require 'ob-latex)
-;; 	      (require 'ob-js)
-;; 	      (require 'ob-css)
-;; 	      (require 'ob-sql)
-;; 	      (require 'ob-plantuml)
-;; 	      (require 'ob-emacs-lisp))))
-
-;; (add-hook 'emacs-startup-hook 'my-org-setup)
+(defun org-current-is-todo ()
+  "Check if the currect node is in todo state."
+  (string= "TODO" (org-get-todo-state)))
 
 (provide 'init-org)
 ;;; init-org.el ends here
