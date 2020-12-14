ece170df87f41c77a0b78c58deff959557b9d5f2
Pankaj Jangid <pankaj.jangid@gmail.com>
2020-12-14 09:37:43 +0530

    visible-bell for mac, audio bell is annoying

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -4,14 +4,15 @@
 
 ;; Probably my right and left switches are swapped inside keyboard.
 
-(when (eq system-type 'darwin)
-  (setq
-   mac-command-modifier 'none
-   mac-right-command-modifier 'meta
-   mac-option-modifier 'none
-   mac-right-option-modifier 'super))
-
-(setq visible-bell nil)
+(cond
+ ((eq system-type 'darwin)
+  (defvar mac-command-modifier 'none)
+  (defvar mac-right-command-modifier 'meta)
+  (defvar mac-option-modifier 'none)
+  (defvar mac-right-option-modifier 'super)
+  (setq visible-bell t))
+ (t
+  (setq visible-bell nil)))
 
 (provide 'init-keys)
 ;;; init-keys.el ends here
