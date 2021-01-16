7e58524b131f17815383c2c8e2656f00a43eebc5
Pankaj Jangid <pankaj.jangid@gmail.com>
2021-01-16 17:51:30 +0530

    forgot to commit

--- emacs.d/lisp/init-auth.el
+++
@@ -1,9 +0,0 @@
-;;; init-auth.el --- auth conf -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(eval-when-compile (require 'auth-source))
-
-
-(provide 'init-auth)
-;;; init-auth.el ends here
