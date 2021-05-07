9c1857ef8ae114cc80261f2ef4b62042e7a6d836
Pankaj Jangid <pankaj@codeisgreat.org>
2021-05-07 08:15:20 +0530

    moved environment settings to early-init.el

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -6,5 +6,33 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 
+;; Environment
+(add-to-list 'exec-path "/sbin")
+(add-to-list 'exec-path "/usr/sbin")
+(add-to-list 'exec-path "/bin")
+(add-to-list 'exec-path "/usr/bin")
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "/Users/pankaj/go/bin")
+(add-to-list 'exec-path "/Library/TeX/texbin")
+(add-to-list 'exec-path "/Library/Apple/usr/bin")
+(add-to-list 'exec-path "/Users/pankaj/.cargo/bin")
+(add-to-list 'exec-path
+             "/Users/pankaj/.sdkman/candidates/ant/current/bin")
+(add-to-list 'exec-path
+             "/Users/pankaj/.sdkman/candidates/gradle/current/bin")
+(add-to-list 'exec-path
+             "/Users/pankaj/.sdkman/candidates/groovy/current/bin")
+(add-to-list 'exec-path
+             "/Users/pankaj/.sdkman/candidates/kotlin/current/bin")
+;; (add-to-list 'exec-path
+;;              "/usr/local/opt/gcc/bin")
+
+(setenv "PATH" (mapconcat 'identity exec-path ":"))
+(setenv "RUST_SRC_PATH"
+    "~/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src/")
+
+(setenv "LANG" "en_US.UTF-8")
+
+
 (provide 'early-init)
 ;;; early-init.el ends here
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -312,33 +312,6 @@
 (custom-set-faces
  )
 
-;; Environment
-(add-to-list 'exec-path "/sbin")
-(add-to-list 'exec-path "/usr/sbin")
-(add-to-list 'exec-path "/bin")
-(add-to-list 'exec-path "/usr/bin")
-(add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'exec-path "/Users/pankaj/go/bin")
-(add-to-list 'exec-path "/Library/TeX/texbin")
-(add-to-list 'exec-path "/Library/Apple/usr/bin")
-(add-to-list 'exec-path "/Users/pankaj/.cargo/bin")
-(add-to-list 'exec-path
-             "/Users/pankaj/.sdkman/candidates/ant/current/bin")
-(add-to-list 'exec-path
-             "/Users/pankaj/.sdkman/candidates/gradle/current/bin")
-(add-to-list 'exec-path
-             "/Users/pankaj/.sdkman/candidates/groovy/current/bin")
-(add-to-list 'exec-path
-             "/Users/pankaj/.sdkman/candidates/kotlin/current/bin")
-;; (add-to-list 'exec-path
-;;              "/usr/local/opt/gcc/bin")
-
-(setenv "PATH" (mapconcat 'identity exec-path ":"))
-(setenv "RUST_SRC_PATH"
-    "~/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src/")
-
-(setenv "LANG" "en_US.UTF-8")
-
 ;; start server for emacsclient support
 (require 'server)
 (run-with-timer
